984deeb5e6930e5f5d34ef1ba3904537e8665137
Marcos Vinicius <marvn.ants1@gmail.com>
2020-09-23 20:34:58 -0300

    test: ensure SignInController.handle return 400 if Validation returns an error

---
+++ src/controllers/signin/signin.spec.ts
@@ -0,0 +1,49 @@
+import { ErrorMessage } from '../../helpers/errors'
+import { badRequest } from '../../helpers/http'
+import { IHttpRequest, IValidation } from '../../helpers/interfaces'
+import { SignInController } from './signin'
+
+interface SutTypes {
+  sut: SignInController
+  validationStub: IValidation
+}
+
+const makeValidation = (): IValidation => {
+  class ValidationStub implements IValidation {
+    validate(input: any): Error {
+      return null
+    }
+  }
+  return new ValidationStub()
+}
+
+const makeFakeRequest = (): IHttpRequest => ({
+  body: {
+    nome: 'any_nome',
+    email: 'any_email@mail.com',
+    senha: 'any_senha',
+    telefones: [{ numero: 123456789, ddd: 11 }]
+  }
+})
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidation()
+
+  const sut = new SignInController(validationStub)
+
+  return { sut, validationStub }
+}
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if Validation returns an error', async () => {
+    const { sut, validationStub } = makeSut()
+
+    jest
+      .spyOn(validationStub, 'validate')
+      .mockReturnValueOnce(new ErrorMessage())
+
+    const httpResponse = await sut.handle(makeFakeRequest())
+
+    expect(httpResponse).toEqual(badRequest(new ErrorMessage()))
+  })
+})
