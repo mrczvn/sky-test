5cb5fa3618654938436ee051fd0b0c7e76a70fec
Marcos Vinicius <marvn.ants1@gmail.com>
2020-09-23 13:51:26 -0300

    chore: move mongodb methods to a helper

---
+++ src/database/mongo-helper.ts
@@ -0,0 +1,37 @@
+import { MongoClient, Collection } from 'mongodb'
+
+export const MongoHelper = {
+  client: null as MongoClient,
+  uri: null as string,
+
+  async connect(uri: string): Promise<void> {
+    this.uri = uri
+
+    this.client = await MongoClient.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  },
+
+  async disconnect(): Promise<void> {
+    await this.client.close()
+
+    this.client = null
+  },
+
+  async getCollection(name: string): Promise<Collection> {
+    if (!this.client?.isConnected()) {
+      await this.connect(this.uri)
+    }
+
+    return this.client.db().collection(name)
+  },
+
+  map: (accountData: any): any => {
+    const { _id, ...accountWithoutId } = accountData
+
+    const accountModel = { ...accountWithoutId, id: _id }
+
+    return accountModel
+  }
+}
