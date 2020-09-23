213a5ceb81f0029b946078b15c4ad3ff7f78ba1a
Marcos Vinicius <marvn.ants1@gmail.com>
2020-09-23 17:06:07 -0300

    test: ensure JwtAdapter.encrypt return a token on sign success

--- src/database/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/database/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -23,4 +23,12 @@ describe('Jwt Adapter', () => {
 
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
   })
+
+  test('Should return a token on sign success', async () => {
+    const sut = makeSut()
+
+    const accessToken = await sut.encrypt('any_id')
+
+    expect(accessToken).toBe('any_token')
+  })
 })
