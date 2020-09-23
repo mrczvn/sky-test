cd4a52c95d2496d4a29c72f6f8b2d52b0c492e51
Marcos Vinicius <marvn.ants1@gmail.com>
2020-09-23 10:10:46 -0300

    test: ensure ValidationComposite returns the first error if more then one validation fails

--- src/helpers/validators/validation-composite.spec.ts
+++ src/helpers/validators/validation-composite.spec.ts
@@ -4,32 +4,49 @@ import { ValidationComposite } from './validation-composite'
 
 interface SutTypes {
   sut: ValidationComposite
-  validationStub: IValidation
+  validationStubs: IValidation[]
 }
 
 const makeValidation = (): IValidation => {
   class ValidationStub implements IValidation {
     validate(input: any): Error {
-      return new ErrorMessage()
+      return null
     }
   }
   return new ValidationStub()
 }
 
 const makeSut = (): SutTypes => {
-  const validationStub = makeValidation()
+  const validationStubs = [makeValidation(), makeValidation()]
 
-  const sut = new ValidationComposite([validationStub])
+  const sut = new ValidationComposite(validationStubs)
 
-  return { sut, validationStub }
+  return { sut, validationStubs }
 }
 
 describe('Validation Composite', () => {
   test('Should return an error if any validation fails', () => {
-    const { sut } = makeSut()
+    const { sut, validationStubs } = makeSut()
+
+    jest
+      .spyOn(validationStubs[0], 'validate')
+      .mockReturnValueOnce(new ErrorMessage())
 
     const error = sut.validate({ field: 'any_field' })
 
     expect(error).toEqual(new ErrorMessage())
+  })
+
+  test('Should return the first error if more then one validation fails', () => {
+    const { sut, validationStubs } = makeSut()
+
+    jest.spyOn(validationStubs[0], 'validate').mockReturnValueOnce(new Error())
+    jest
+      .spyOn(validationStubs[1], 'validate')
+      .mockReturnValueOnce(new ErrorMessage())
+
+    const error = sut.validate({ field: 'any_field' })
+
+    expect(error).toEqual(new Error())
   })
 })
