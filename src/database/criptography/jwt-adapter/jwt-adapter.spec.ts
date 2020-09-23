46ac2d3a9966fdb9928190298fa1e95c857136aa
Marcos Vinicius <marvn.ants1@gmail.com>
2020-09-23 17:17:05 -0300

    test: ensure JwtAdapter.decrypt throw if verify throw

--- src/database/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/database/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -61,4 +61,16 @@ describe('Jwt Adapter', () => {
 
     expect(value).toBe('any_value')
   })
+
+  test('Should throw if verify throws', async () => {
+    const sut = makeSut()
+
+    jest.spyOn(jwt, 'verify').mockImplementationOnce(() => {
+      throw new Error()
+    })
+
+    const value = sut.decrypt('any_token')
+
+    await expect(value).rejects.toThrow()
+  })
 })
