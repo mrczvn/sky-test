475a351f9b905c780d6c7973084c0008e097206b
Marcos Vinicius <marvn.ants1@gmail.com>
2020-09-30 11:18:58 -0300

    test: ensure GetAccountController.handle return 403 if LoadAccountById returns null

--- src/controllers/account/get-account.spec.ts
+++ src/controllers/account/get-account.spec.ts
@@ -1,42 +1,42 @@
-import {
-  IAccountModel,
-  IHttpRequest,
-  ILoadAccountByIdRepository
-} from '../../helpers/interfaces'
+import { AccessDeniedError } from '../../helpers/errors/access-denied-error'
+import { forbidden } from '../../helpers/http'
+import { IAccount, IHttpRequest } from '../../helpers/interfaces'
+import { ILoadAccountById } from '../../helpers/interfaces/db/load-account-by-id'
 import { GetAccountController } from './get-account'
 
 interface SutTypes {
   sut: GetAccountController
-  loadAccountByIdStub: ILoadAccountByIdRepository
+  loadAccountByIdStub: ILoadAccountById
 }
 
-const makeLoadAccountById = (timestamp): ILoadAccountByIdRepository => {
-  class LoadAccountByIdStub implements ILoadAccountByIdRepository {
-    async loadById(id: string): Promise<IAccountModel> {
-      return makeFakeAccount(timestamp)
+const makeLoadAccountById = (): ILoadAccountById => {
+  class LoadAccountByIdStub implements ILoadAccountById {
+    async loadById(id: string): Promise<IAccount> {
+      return new Promise((resolve) => resolve(makeFakeAccount()))
     }
   }
   return new LoadAccountByIdStub()
 }
 
-const makeFakeAccount = (timestamp): IAccountModel => ({
+const makeFakeAccount = (
+  creationDate = new Date('2020-09-29 12:00'),
+  dataUpdate = new Date('2020-09-29 14:00')
+): IAccount => ({
   id: 'any_id',
   nome: 'any_nome',
   email: 'any_email@mail.com',
   senha: 'any_senha',
   telefones: [{ ddd: 11, numero: 123456789 }],
-  data_criacao: timestamp,
-  data_atualizacao: timestamp,
-  ultimo_login: timestamp,
+  data_criacao: creationDate,
+  data_atualizacao: dataUpdate,
+  ultimo_login: dataUpdate,
   token: 'any_token'
 })
 
-const makeFakeRequest = (account): IHttpRequest => ({
-  user: account
-})
+const makeFakeRequest = (account: IAccount): IHttpRequest => ({ user: account })
 
-const makeSut = (timestamp = new Date()): SutTypes => {
-  const loadAccountByIdStub = makeLoadAccountById(timestamp)
+const makeSut = (): SutTypes => {
+  const loadAccountByIdStub = makeLoadAccountById()
 
   const sut = new GetAccountController(loadAccountByIdStub)
 
@@ -45,16 +45,28 @@ const makeSut = (timestamp = new Date()): SutTypes => {
 
 describe('GetAccount Controller', () => {
   test('Should call LoadAccountById with correct id', async () => {
-    const date = new Date()
-
-    const { sut, loadAccountByIdStub } = makeSut(date)
+    const { sut, loadAccountByIdStub } = makeSut()
 
     const loadByIdSpy = jest.spyOn(loadAccountByIdStub, 'loadById')
 
-    const httpRequest = makeFakeRequest(makeFakeAccount(date))
+    const httpRequest = makeFakeRequest(makeFakeAccount())
 
     await sut.handle(httpRequest)
 
     expect(loadByIdSpy).toHaveBeenCalledWith(httpRequest.user.id)
+  })
+
+  test('Should return 403 if LoadAccountById returns null', async () => {
+    const { sut, loadAccountByIdStub } = makeSut()
+
+    jest.spyOn(loadAccountByIdStub, 'loadById').mockResolvedValueOnce(null)
+
+    const httpRequest = makeFakeRequest(makeFakeAccount())
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse).toEqual(
+      forbidden(new AccessDeniedError('Sessão inválida'))
+    )
   })
 })
--- src/controllers/account/get-account.ts
+++ src/controllers/account/get-account.ts
@@ -3,28 +3,28 @@ import { forbidden, ok, serverError } from '../../helpers/http'
 import {
   IController,
   IHttpRequest,
-  IHttpResponse,
-  ILoadAccountByIdRepository
+  IHttpResponse
 } from '../../helpers/interfaces'
+import { ILoadAccountById } from '../../helpers/interfaces/db/load-account-by-id'
 import { compareDate } from '../../utils/compare-date'
 import { transformeAccountModel } from '../../utils/transforme-account-model'
 
 export class GetAccountController implements IController {
-  constructor(private readonly loadAccountById: ILoadAccountByIdRepository) {}
+  constructor(private readonly loadAccountById: ILoadAccountById) {}
 
   async handle(req: IHttpRequest): Promise<IHttpResponse> {
     try {
-      const account = req.user
+      const account = await this.loadAccountById.loadById(req.user.id)
 
       if (account) {
-        const date = new Date()
+        const atThisMoment = new Date()
 
-        const differenceDate = compareDate(date, account.ultimo_login)
+        const differenceDate = compareDate(atThisMoment, account.ultimo_login)
 
         if (differenceDate) return ok(transformeAccountModel(account))
-
-        return forbidden(new AccessDeniedError('Sessão inválida'))
       }
+
+      return forbidden(new AccessDeniedError('Sessão inválida'))
     } catch (error) {
       return serverError()
     }
